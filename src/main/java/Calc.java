5f6ea0d119558609a6499ca3304e4fffa0a51d70
Natasha093 <fanloko063@mail.ru>
2018-05-27 14:31:42 +0300

    nytdhyutd

---
+++ src/main/java/Calc.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+/**
+ * Калькулятор
+ * @author Ежова Наталья
+ */
+public class Calc {
+    public static void main (String [] args) throws IOException {
+        Scanner scanner = new Scanner (System.in);
+
+        float d = 0;
+
+        System.out.println ("Введите 1-е число:");
+        float a = scanner.nextFloat ();
+        System.out.println ("Оператор:");
+        char b = scanner.next().charAt(0);
+        System.out.println ("Введите 2-е число");
+        float c = scanner.nextFloat ();
+        //Операция сложения
+        if (b == '+'){
+            d = a + c;
+        }
+        //Операция вычитания
+        else if (b == '-') {
+            d = a - c;
+        }
+        //Операция умножения
+        else if (b == '*') {
+            d = a * c;
+        }
+        //Операция деления
+        else if (b == '/') {
+            d = a / c;
+        }
+        //Вывод результата
+        System.out.printf("Результат: " + "%.4f", d);
+    }
+}
